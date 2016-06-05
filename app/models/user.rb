423ef4879a28e03aab76424ceb57c2af4c0ee33b
Alex Frederick <afreder0@users.noreply.github.com>
2016-06-04 22:48:17 -0500

    NoMethodError

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :favorites , :class_name => "Favorite", :foreign_key => "user_id"
-         has_many :pics
+         has_many :pics , :class_name => "Pic", :foreign_key => "user_id"
 end
