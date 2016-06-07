7a8c3833e045c5065a35c4a6a3392417427b4c8b
Alex Frederick <afreder0@users.noreply.github.com>
2016-06-06 22:04:27 -0500

    Updates

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,6 @@
 class Dish < ActiveRecord::Base
 has_many :dishings , :class_name => "Dishing", :foreign_key => "dish_id"
-
+  has_many :taggings , :class_name => "Tagging", :foreign_key => "dish_id"
 
 
 end
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,6 +1,6 @@
 class Tagging < ActiveRecord::Base
 
-  belongs_to :dishing , :class_name => "Dishing", :foreign_key => "dishing_id"
+  belongs_to :dish , :class_name => "Dish", :foreign_key => "dish_id"
 
   belongs_to :tag , :class_name => "Tag", :foreign_key => "tag_id"
 
