bca06407563227517c656a5389d17e9f7309bc4d
Alex Frederick <afreder0@users.noreply.github.com>
2016-06-09 21:45:40 -0500

    Not bad

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
 has_many :dishings , :class_name => "Dishing", :foreign_key => "restaurant_id", :dependent => :destroy
 validates :city, :presence => true
+has_and_belongs_to_many :dishes, join_table: :dishings
 
 end
